eed4b74eb1f1c6daeb7d88c9fe5d9bba6047952c
Vashu Singh <rajputvashusingh@gmail.com>
2025-07-05 01:16:45 +0530

    Udated start.sh

--- Backend/start.sh
+++ Backend/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python --version  # 👈 add this to debug
-uvicorn app.main:app --host 0.0.0.0 --port 10000
+set -o errexit
+pip install -r requirements.txt
+python -m uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-10000}
